53a3bd4db5edfe02fbb56da6c430fbcb852f62c5
Duane Hodges-Stubbs <dstubbs@solutionlabs.co.uk>
2020-04-27 12:45:42 +0100

    Added comments and different pubsub types.

--- base64.go
+++ base64.go
@@ -2,6 +2,7 @@ package solutionlabs_tools
 
 import "encoding/base64"
 
+// Base64ToString converts a base64 encoded string to a standard string
 func Base64ToString(str string) (string, error) {
 	d, err := base64.StdEncoding.DecodeString(str)
 	return string(d), err
--- pubsub.go
+++ pubsub.go
@@ -11,8 +11,6 @@ import (
 	"time"
 )
 
-// {"message":{"attributes":{"source":"parkon"}, "data":"eyJzaXRlaWQiOiI4Mjc4IiwidnJtIjoiVEVTVDQiLCJmcm9tIjoiMjAyMC0wNC0yNyAxMjowMCIsInRvIjoiMjAyMC0wNC0yNyAxMjowNSIsImxhYmVsIjoiIiwiYWN0aW9uIjoiQUREIiwiY2xpZW50aWQiOiIxNzAxOCIsInJlbW90ZV90a24iOiIxMzIiLCJ1c2VyaWQiOiIxIn0=", "messageId":"1149481834963349","message_id":"1149481834963349","publishTime":"2020-04-27T10:59:01.995Z","publish_time":"2020-04-27T10:59:01.995Z"},"subscription":"projects/paymypcn-backoffice/subscriptions/vrmlist_2_whitelist_api"}
-
 type PubSubMessage struct {
 	Attributes   map[string]string
 	Data         string
@@ -21,7 +19,27 @@ type PubSubMessage struct {
 	Subscription string
 }
 
+// ReadMessageFromPost takes the HTTP request feed and returns PubSubMessage struct
+// converts time string to time object
 func ReadMessageFromPost(r *http.Request) (message PubSubMessage, err error) {
+
+	body, _ := ioutil.ReadAll(r.Body)
+
+	if len(body) == 0 {
+		return message, errors.New("missing body")
+	}
+
+	return ReadMessageFromByte(body)
+
+}
+
+// ReadMessageFromString takes string feed and returns PubSubMessage struct
+func ReadMessageFromString(body string) (message PubSubMessage, err error) {
+	return ReadMessageFromByte([]byte(body))
+}
+
+// ReadMessageFromString takes []byte feed and returns PubSubMessage struct
+func ReadMessageFromByte(body []byte) (message PubSubMessage, err error) {
 
 	var msg = struct {
 		Message struct {
@@ -32,12 +50,6 @@ func ReadMessageFromPost(r *http.Request) (message PubSubMessage, err error) {
 		} `json:"message"`
 		Subscription string `json:"subscription"`
 	}{}
-
-	body, _ := ioutil.ReadAll(r.Body)
-
-	if len(body) == 0 {
-		return message, errors.New("missing body")
-	}
 
 	err = json.Unmarshal(body, &msg)
 
